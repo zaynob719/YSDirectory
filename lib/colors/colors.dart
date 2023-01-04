2e856940a40b34513cef294b466864da22c02763
Zaynob <74244679+zaynob719@users.noreply.github.com>
2023-01-04 17:09:00 +0000

    delete

--- lib/colors/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static final Color textColor = const Color(0x000000);
-  static final Color secondBrownColor = const Color(0xFFD8BA9E);
-  static final Color mainBrownColor = const Color(0xFFF1E6DB);
-}
