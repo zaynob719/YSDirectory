6965e94e6645d0bdbb35fa5617acd0b0ca5c1ee6
Zaynob <74244679+zaynob719@users.noreply.github.com>
2023-01-06 00:20:31 +0000

    06-01-22

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static final Color textColor = const Color(0x000000);
   static final Color secondBrownColor = const Color(0xFFD8BA9E);
   static final Color mainBrownColor = const Color(0xFFF1E6DB);
+  static final Color white = const Color(0xFFFFFF);
 }
