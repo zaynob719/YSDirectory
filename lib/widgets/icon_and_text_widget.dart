d061e497a6fa9e424614164d77c70f7ec3034f6a
Zaynob <74244679+zaynob719@users.noreply.github.com>
2023-01-12 17:27:54 +0000

    12/01/23

--- lib/widgets/icon_and_text_widget.dart
+++ lib/widgets/icon_and_text_widget.dart
@@ -8,12 +8,12 @@ class IconAndTextWidget extends StatelessWidget {
   final IconData icon;
   final String text;
   final Color iconColor;
-  const IconAndTextWidget(
-      {Key? key,
-      required this.icon,
-      required this.text,
-      required this.iconColor})
-      : super(key: key);
+  const IconAndTextWidget({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.iconColor,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
