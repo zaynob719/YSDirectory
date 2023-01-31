37e8f8fc124b5008040053d36bca7618be4383cc
Zaynob <74244679+zaynob719@users.noreply.github.com>
2023-01-31 18:33:17 +0000

    modifications

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -23,9 +23,10 @@ class Dimensions {
   static double height30 = screenHeight / 28.13;
   static double height40 = screenHeight / 21.1;
 
-  //font
+  //font size
   static double font20 = screenHeight / 42.2;
   static double font26 = screenHeight / 32.46;
+  static double font16 = screenHeight / 52.75;
 
   //radious
   static double radius20 = screenHeight / 42.2;
--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -8,12 +8,14 @@ class AppIcon extends StatelessWidget {
   final Color backgroudColor;
   final Color iconColor;
   final double size;
+  final double iconSize;
   AppIcon(
       {Key? key,
       required this.icon,
       this.backgroudColor = const Color(0xFFFFFFFF),
       this.iconColor = const Color(0xFF000000),
-      this.size = 40})
+      this.size = 40,
+      this.iconSize = 16})
       : super(key: key);
 
   @override
@@ -26,7 +28,7 @@ class AppIcon extends StatelessWidget {
       child: Icon(
         icon,
         color: iconColor,
-        size: Dimensions.iconSize16,
+        size: iconSize,
       ),
     );
   }
