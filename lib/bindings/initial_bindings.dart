85c583138fcc906847257545fb6e2f7a75fafc74
Zaynob <74244679+zaynob719@users.noreply.github.com>
2023-02-19 16:35:03 +0000

    updated images

--- lib/bindings/initial_bindings.dart
+++ lib/bindings/initial_bindings.dart
@@ -1,9 +1,13 @@
 import 'package:coveredncurly/controllers/auth_controller.dart';
 import 'package:get/get.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+import '../services/firebase_storage_service.dart';
 
 class InitialBindings implements Bindings {
   @override
   void dependencies() {
     Get.put(AuthController(), permanent: true);
+    Get.lazyPut(() => FireBaseStorageService());
   }
 }
