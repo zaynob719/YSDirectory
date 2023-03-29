344b9b62ae45bbfdecb2b9c0616605e9e6e3d5b0
Zaynob <74244679+zaynob719@users.noreply.github.com>
2023-03-29 16:45:20 +0100

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:coveredncurly/layout/screen_layout.dart';
 import 'package:coveredncurly/routes/app_routes.dart';
 import 'package:coveredncurly/screens/pages/home_salon_page.dart';
 import 'package:coveredncurly/screens/introduction/introduction.dart';
-import 'package:coveredncurly/screens/pages/main_page.dart';
 import 'package:coveredncurly/screens/pages/salon_page_body.dart';
 import 'package:coveredncurly/screens/salon/main_salon_detail.dart';
 import 'package:coveredncurly/screens/sign_in_screen/sign_in_screen.dart';
