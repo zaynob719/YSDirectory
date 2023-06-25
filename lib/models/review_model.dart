b03cdffd1a9d390a4fdce6ccbcfcefa0c6b9ac81
Zaynob <74244679+zaynob719@users.noreply.github.com>
2023-06-25 23:24:19 +0100

    25/06/23

--- lib/models/review_model.dart
+++ lib/models/review_model.dart
@@ -1,25 +1,25 @@
 class ReviewModel {
   final String senderName;
   final String reviewController;
-  final int rating;
+  final String rating;
   final String attendanceDate;
-  final String titleController;
+  //final String titleController;
 
   const ReviewModel({
     required this.senderName,
     required this.reviewController,
     required this.rating,
     required this.attendanceDate,
-    required this.titleController,
+    //required this.titleController,
   });
 
   factory ReviewModel.fromJson(Map<String, dynamic> json) {
     return ReviewModel(
       senderName: json["senderName"] as String? ?? '',
       reviewController: json["reviewController"] as String? ?? '',
-      rating: json["rating"] as int? ?? 0,
+      rating: json["rating"] as String? ?? '',
       attendanceDate: json["attendanceDate"] as String? ?? '',
-      titleController: json["titleController"] as String? ?? '',
+      //titleController: json["titleController"] as String? ?? '',
     );
   }
 
@@ -29,7 +29,7 @@ class ReviewModel {
       'reviewController': reviewController,
       'rating': rating,
       'attendanceDate': attendanceDate,
-      'titleController': titleController,
+      //'titleController': titleController,
     };
   }
 }
